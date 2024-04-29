d911a2f1c322915ecc916f4a3ef271ae8797c983
Alessandro <sandroledesma@gmail.com>
2024-04-29 13:17:06 -0400

    update files

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  const highPriorityList = document.querySelector("#high-priority");
-  const midPriorityList = document.querySelector("#mid-priority");
-  const lowPriorityList = document.querySelector("#low-priority");
+  const highPriorityList = document.querySelector("#HighPriorityList");
+  const midPriorityList = document.querySelector("#MidPriorityList");
+  const lowPriorityList = document.querySelector("#LowPriorityList");
 
   document.querySelector("form").addEventListener("submit", (event) => {
     event.preventDefault();
     const tasks = event.target["new-task-description"].value;
     const list = document.createElement("li");
-    //const form = document.querySelector("form"); // FIX THE PRIORITY CODE
     list.textContent = tasks;
 
     if(tasks.trim() === "") // this code does not allow blank text to be created as a todo - however, it gives you an error.
     {
-      e.preventDefault(); 
+      return; 
     }
   
     const deleteButton = document.createElement("button"); // creates delete button to remove the task once complete
@@ -30,24 +29,28 @@ document.addEventListener("DOMContentLoaded", (event) => {
     event.target["new-task-description"].value = ""; // resets the text box to placeholder = description
 
     const priority = event.target.priority.value; 
-      if (priority === "high") list.style.color = "red";
-      if (priority === "mid") list.style.color = "blue";
-      if (priority === "low") list.style.color = "green";
+      if (priority === "high") 
+      if (priority === "mid") 
+      if (priority === "low") 
     
     document.querySelector("#tasks").appendChild(list);
 
     switch (priority) { //execute a switch statement to place tasks in appropriate priority list
       case "high":
-        highPriorityList.appendChild(tasks);
+        list.style.color = "red";
+        highPriorityList.appendChild(list);
         break;
       case "mid":
-        midPriorityList.appendChild(tasks);
+        list.style.color = "blue";
+        midPriorityList.appendChild(list);
         break;   
       case "low":
-        lowPriorityList.appendChild(tasks);
+        list.style.color = "green";
+        lowPriorityList.appendChild(list);
         break;
       default: //if no priority was made, it will default to Low
-        lowPriorityList.appendChild(tasks);
+        list.style.color = "black";
+        lowPriorityList.appendChild(list);
         break;
     }
     
